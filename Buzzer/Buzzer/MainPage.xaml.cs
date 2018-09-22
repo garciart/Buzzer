9771e6a3f7d304ab5a1c25bf5627a160a42dbc50
garciart <rgarcia@rgprogramming.com>
2018-09-22 16:32:33 -0400

    Initial commit of Buzzer Tutorial

--- Buzzer/Buzzer/MainPage.xaml.cs
+++ Buzzer/Buzzer/MainPage.xaml.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.IO;
+using System.Reflection;
 using Xamarin.Forms;
 
 namespace Buzzer {
     public partial class MainPage : ContentPage {
         public MainPage() {
             InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e) {
+            ISimpleAudioPlayer player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            player.Load(GetStreamFromFile("buzzer.mp3"));
+            player.Play();
+        }
+
+        private Stream GetStreamFromFile(string filename) {
+            Assembly assembly = typeof(App).GetTypeInfo().Assembly;
+            Stream stream = assembly.GetManifestResourceStream("Buzzer.Resources." + filename);
+            return stream;
         }
     }
 }
